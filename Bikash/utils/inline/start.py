78f312c8d827002fa53d48b79661702b3caa0003
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-01-04 00:19:06 +0400

    Update start.py

--- Bikash/utils/inline/start.py
+++ Bikash/utils/inline/start.py
@@ -21,28 +21,3 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
      ]
     return buttons
-
-
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="❀⋟ ʜᴇʟᴘ ⋞❀", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="✭ ᴜᴘᴅᴀᴛᴇs ✭", url=config.SUPPORT_CHANNEL
-            ),
-            InlineKeyboardButton(
-                text="✭ sᴜᴘᴘᴏʀᴛ ✭", url=config.SUPPORT_GROUP
-            )
-        ],
-     ]
-    return buttons
